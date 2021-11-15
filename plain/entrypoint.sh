972b5eb8aac023a7a056469fdf2ed8e282212c0b
Laica Lunasys <laica.lunasys@gmail.com>
2021-11-15 09:38:51 +0900

    Update to 1.17.1

--- plain/entrypoint.sh
+++ plain/entrypoint.sh
@@ -4,15 +4,15 @@ set -e
 # Initialize Running user
 if [ $(id -u) = 0 ]; then
     # Switch provided user
-    if [[ -v UID ]] && [ "$UID" != 0 ] && [[ $UID != $(id -u app) ]]; then
+    if [ "${UID:+defined}" ] && [ "$UID" != 0 ] && [[ $UID != $(id -u app) ]]; then
         echo ":: $UID"
         usermod -u $UID app
     fi
-    if [[ -v GID ]] && [ "$GID" != 0 ] && [[ $GID != $(id -g app) ]]; then
+    if [ "${GID:+defined}" ] && [ "$GID" != 0 ] && [[ $GID != $(id -g app) ]]; then
         echo ":: $GID"
         groupmod -o -g $GID app
     fi
 fi
 
 chown ${UID:-app}:${GID:-app} -R /app/
-exec su-exec ${UID:-app} bash /starter.sh
+exec su-exec ${UID:-app} sh /starter.sh
--- plain/starter.sh
+++ plain/starter.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 set -e
-cd /app/server
 
+# Auto configuration
+echo "Running pre-configuration..."
+cd /app/config
+sh /autoconfig.sh
+
+echo "Starting server..."
+cd /app/server
 exec java \
     -Dcom.mojang.eula.agree=true \
     -jar /paper/paper.jar \
     --nogui \
-    --server-name="${SERVER_NAME-unknown}" \
-    --online-mode "${ONLINE_MODE-true}" \
-    --port "${PORT-25565}" \
-    --max-players "${MAX_PLAYERS-100}" \
+    --server-name="${SERVER_NAME:-unknown}" \
+    --port="${PORT:-25565}" \
+    --max-players="${MAX_PLAYERS:-100}" \
     --bukkit-settings /app/config/bukkit.yml \
     --commands-settings /app/config/commands.yml \
     --spigot-settings /app/config/spigot.yml \
